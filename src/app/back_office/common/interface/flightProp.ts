b357ea63b5f8052084b140496ae84e4dab90d2b4
nadir inab <nadir.inab.dev@gmail.com>
2024-04-02 21:42:22 +0000

    common is added

---
+++ src/app/back_office/common/interface/flightProp.ts
@@ -0,0 +1,11 @@
+export interface Flight {
+    flightName: string;
+    date: string;
+    from: string;
+    to: string;
+    airport: string;
+    airline: string;
+    price: number;
+    seat: string;
+  }
+  
